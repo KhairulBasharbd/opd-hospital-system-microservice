387f0bdb03522988bec0f6a77e82c4084b8e6b16
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-10 17:20:59 +0600

    Create User Entity

--- opd-auth-service/src/main/java/com/ztrios/opd_auth_service/entity/UserEntity.java
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/entity/UserEntity.java
@@ -1,4 +1,57 @@
 package com.ztrios.opd_auth_service.entity;
 
+import com.ztrios.opd_auth_service.entity.PatientProfileEntity;
+import com.ztrios.opd_auth_service.enums.Role;
+import com.ztrios.opd_auth_service.enums.Status;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Builder
+@Data
+@NoArgsConstructor
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = true)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    private String phone;
+
+    @Column(nullable = false)
+    private Role role;
+
+    private Status status;
+
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = true)
+    private Instant updatedAt ;
+
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = true)
+    private PatientProfileEntity patientProfile;
+
 }
+
