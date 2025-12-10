61c91eee7da9b3eed2811636fdb54ccaafa8cc36
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-10 17:19:57 +0600

    Create BloodGroup enums

---
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/enums/BloodGroup.java
@@ -0,0 +1,27 @@
+package com.ztrios.opd_auth_service.enums;
+
+public enum BloodGroup {
+    A_POSITIVE("A+"),
+    A_NEGATIVE("A-"),
+    B_POSITIVE("B+"),
+    B_NEGATIVE("B-"),
+    AB_POSITIVE("AB+"),
+    AB_NEGATIVE("AB-"),
+    O_POSITIVE("O+"),
+    O_NEGATIVE("O-");
+
+    private final String displayName;
+
+    BloodGroup(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+}
