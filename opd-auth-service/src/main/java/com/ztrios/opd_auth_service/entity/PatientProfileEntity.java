16a3a86ad9a4af0d5eef71ae955aeef92be4677a
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-11 17:07:00 +0600

    Changed primary key generation type Identity to Auto

--- opd-auth-service/src/main/java/com/ztrios/opd_auth_service/entity/PatientProfileEntity.java
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/entity/PatientProfileEntity.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class PatientProfileEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @OneToOne
