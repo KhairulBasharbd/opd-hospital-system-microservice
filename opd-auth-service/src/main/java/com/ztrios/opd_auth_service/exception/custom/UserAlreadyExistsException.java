5cb6761316277a8442dea73d7adb170ba896ed6d
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-11 17:07:26 +0600

    Add UserAlreadyExistsException

---
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/exception/custom/UserAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.ztrios.opd_auth_service.exception.custom;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
