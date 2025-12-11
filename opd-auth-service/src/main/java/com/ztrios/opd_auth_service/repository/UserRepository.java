7a86ab9d08149c20533166efa8a947991e04d2ea
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-11 17:07:59 +0600

    Add Query

--- opd-auth-service/src/main/java/com/ztrios/opd_auth_service/repository/UserRepository.java
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.ztrios.opd_auth_service.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository <UserEntity, UUID> {
-
-
+        Boolean existsByEmail(String email);
+        Optional<UserEntity> findByEmail(String email);
 }
