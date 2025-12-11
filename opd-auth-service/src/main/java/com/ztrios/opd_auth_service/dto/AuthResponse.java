21279f2e615fe066cf391dbae805abb745b7f026
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-11 17:08:19 +0600

    Rename RegisterPatientResponse

--- opd-auth-service/src/main/java/com/ztrios/opd_auth_service/dto/AuthResponse.java
+++
@@ -1,3 +0,0 @@
-package com.ztrios.opd_auth_service.dto;
-
-public record AuthResponse(String token, String message) {}
---
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/dto/RegisterPatientResponse.java
@@ -0,0 +1,3 @@
+package com.ztrios.opd_auth_service.dto;
+
+public record RegisterPatientResponse(String token, String message) {}
