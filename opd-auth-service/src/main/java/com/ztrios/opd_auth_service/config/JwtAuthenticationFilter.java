2e85cb63bbcc0cbf26d37733808ccb49b6d30c32
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-11 17:05:46 +0600

    Create JwtAuthenticationFilter

---
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/config/JwtAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.ztrios.opd_auth_service.config;
+
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        try {
+            Claims claims = jwtUtil.extractClaims(token);
+
+            String userId = claims.getSubject();
+            String role = claims.get("role", String.class);
+
+            List<GrantedAuthority> authorities =
+                    List.of(new SimpleGrantedAuthority(role));
+
+            Authentication auth =
+                    new UsernamePasswordAuthenticationToken(userId, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+
+        } catch (Exception ex) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.getWriter()
+                    .write("{\"error\":\"Invalid or expired token\"}");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
