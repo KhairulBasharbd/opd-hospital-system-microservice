f4f3b78fafa067aea4579c4137a768ea37df62c6
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-11 17:50:40 +0600

    Add Query in PatientProfileRepository

--- opd-auth-service/src/main/java/com/ztrios/opd_auth_service/repository/PatientProfileRepository.java
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/repository/PatientProfileRepository.java
@@ -4,10 +4,11 @@ import com.ztrios.opd_auth_service.entity.PatientProfileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PatientProfileRepository extends JpaRepository<PatientProfileEntity, UUID> {
 
-
+    Optional<PatientProfileEntity> findByUserId(UUID id);
 }
