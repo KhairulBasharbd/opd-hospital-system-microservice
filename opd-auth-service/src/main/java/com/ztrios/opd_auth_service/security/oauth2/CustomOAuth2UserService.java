01500852874e3c95a724f1586039e5d3d55b9c59
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-15 15:41:49 +0600

    Create user and Patient entity both in oauth login

--- opd-auth-service/src/main/java/com/ztrios/opd_auth_service/security/oauth2/CustomOAuth2UserService.java
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/security/oauth2/CustomOAuth2UserService.java
@@ -1,8 +1,11 @@
 package com.ztrios.opd_auth_service.security.oauth2;
 
 
+import com.ztrios.opd_auth_service.entity.PatientProfileEntity;
 import com.ztrios.opd_auth_service.entity.UserEntity;
 import com.ztrios.opd_auth_service.enums.Role;
+import com.ztrios.opd_auth_service.enums.Status;
+import com.ztrios.opd_auth_service.repository.PatientProfileRepository;
 import com.ztrios.opd_auth_service.repository.UserRepository;
 import com.ztrios.opd_auth_service.security.oauth2userinfo.OAuth2UserInfo;
 import com.ztrios.opd_auth_service.security.oauth2userinfo.OAuth2UserInfoFactory;
@@ -14,15 +17,18 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
+    private final PatientProfileRepository patientProfileRepository;
 
-    public CustomOAuth2UserService(UserRepository userRepository) {
+    public CustomOAuth2UserService(UserRepository userRepository, PatientProfileRepository patientProfileRepository) {
         this.userRepository = userRepository;
+        this.patientProfileRepository = patientProfileRepository;
     }
 
     @Override
@@ -45,9 +51,23 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                     newUser.setEmail(userInfo.getEmail());
                     newUser.setFullName(userInfo.getName());
                     newUser.setRole(Role.PATIENT);
+                    newUser.setStatus(Status.ACTIVE);
+                    Instant now = Instant.now();
+                    newUser.setCreatedAt(now);
+                    newUser.setUpdatedAt(now);
+
+                    PatientProfileEntity patient = new PatientProfileEntity();
+                    patient.setUser(newUser);
+
+                    newUser.setPatientProfile(patient);
 
                     //newUser.setProvider(AuthProvider.GOOGLE);
-                    return userRepository.save(newUser);
+
+                    UserEntity savedUser = userRepository.save(newUser);
+                    patientProfileRepository.save(patient);
+
+                    return savedUser;
+
                 });
 
         // 2️⃣ Return Spring Security principal
