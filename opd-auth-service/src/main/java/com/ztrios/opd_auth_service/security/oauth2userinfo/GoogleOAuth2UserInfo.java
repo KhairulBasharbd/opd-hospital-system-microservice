2bf4be2f3b229896c7d4b6897bd85e91d2b38cbc
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-15 14:38:00 +0600

    Create GoogleOAuth2UserInfo

---
+++ opd-auth-service/src/main/java/com/ztrios/opd_auth_service/security/oauth2userinfo/GoogleOAuth2UserInfo.java
@@ -0,0 +1,29 @@
+package com.ztrios.opd_auth_service.security.oauth2userinfo;
+
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo implements OAuth2UserInfo {
+
+    private final Map<String, Object> attributes;
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("sub");
+    }
+}
+
