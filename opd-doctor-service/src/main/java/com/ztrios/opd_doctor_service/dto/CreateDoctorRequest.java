6591b9d7ae7f1b86ed0cedf3945b44b7ec181a0c
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-17 17:57:02 +0600

    Add new field in CreateDoctorRequest record

--- opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/dto/CreateDoctorRequest.java
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/dto/CreateDoctorRequest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public record CreateDoctorRequest(
+                                  UUID userId,
                                   String degree,
                                   Specialization specialization,
                                   Integer experienceYears,
