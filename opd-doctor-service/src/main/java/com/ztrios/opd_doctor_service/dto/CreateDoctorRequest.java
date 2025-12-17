c17a2a9677a69259a358b32e1b3876ec02cf2ebc
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-17 16:11:58 +0600

    Create CreateDoctorRequest dto

---
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/dto/CreateDoctorRequest.java
@@ -0,0 +1,16 @@
+package com.ztrios.opd_doctor_service.dto;
+
+import com.ztrios.opd_doctor_service.enums.DoctorStatus;
+import com.ztrios.opd_doctor_service.enums.Specialization;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record CreateDoctorRequest(
+                                  String degree,
+                                  Specialization specialization,
+                                  Integer experienceYears,
+                                  String licenseNumber,
+                                  BigDecimal consultationFee,
+                                  DoctorStatus status,
+                                  String bio) { }
