d15324571fbe0dbfbfc1852a7ebb4a4e88eaf3e6
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-17 16:08:35 +0600

    Create DoctorScheduleEntity

---
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/entity/DoctorScheduleEntity.java
@@ -0,0 +1,61 @@
+package com.ztrios.opd_doctor_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "doctor_schedules",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"doctor_id", "day_of_week", "start_time", "end_time"}
+        ))
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class DoctorScheduleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "doctor_id", nullable = false)
+    private DoctorEntity doctor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "day_of_week")
+    private DayOfWeek dayOfWeek;
+
+    @Column(name = "start_time")
+    private LocalTime startTime;
+
+    @Column(name = "end_time")
+    private LocalTime endTime;
+
+    @Column(name = "maximum_no_of_patient")
+    private int maxPatients;
+
+    @Column(name = "no_of_appointed_patient")
+    private int appointedPatients = 0;
+
+
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = true)
+    private LocalDateTime updatedAt;
+}
