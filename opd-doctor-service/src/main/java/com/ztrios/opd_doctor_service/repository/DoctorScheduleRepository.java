a8f60e5a834ab489b86bda5b7ef20a356c5cc693
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-17 18:00:42 +0600

    Add a query in DoctorScheduleRepository

--- opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorScheduleRepository.java
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorScheduleRepository.java
@@ -6,10 +6,12 @@ import com.ztrios.opd_doctor_service.entity.DoctorScheduleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DoctorScheduleRepository extends JpaRepository<DoctorScheduleEntity, UUID> {
 
+    List<DoctorScheduleEntity>    findByDoctorId(UUID doctorId);
 
 }
