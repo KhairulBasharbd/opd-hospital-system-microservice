421ea2a376cbb734b26ac9d36037ae41a1ccf069
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-17 16:09:16 +0600

    Create DoctorScheduleRepository

---
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorScheduleRepository.java
@@ -0,0 +1,15 @@
+package com.ztrios.opd_doctor_service.repository;
+
+
+import com.ztrios.opd_doctor_service.entity.DoctorEntity;
+import com.ztrios.opd_doctor_service.entity.DoctorScheduleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface DoctorScheduleRepository extends JpaRepository<DoctorScheduleEntity, UUID> {
+
+
+}
