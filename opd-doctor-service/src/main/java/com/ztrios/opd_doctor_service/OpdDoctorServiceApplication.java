31faafe4d1814edee726a7324fb5a1bab2e290ab
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-18 17:51:11 +0600

    Add feign client annotation in OpdDoctorServiceApplication

--- opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/OpdDoctorServiceApplication.java
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/OpdDoctorServiceApplication.java
@@ -2,8 +2,10 @@ package com.ztrios.opd_doctor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OpdDoctorServiceApplication {
 
 	public static void main(String[] args) {
