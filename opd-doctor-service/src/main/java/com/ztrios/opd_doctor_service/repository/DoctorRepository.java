1b94d321bbef83b3dc2ecc80d95c59a10386e467
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-18 17:52:40 +0600

    Add query in DoctorRepository

--- opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorRepository.java
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorRepository.java
@@ -4,9 +4,12 @@ import com.ztrios.opd_doctor_service.entity.DoctorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
 @Repository
 public interface DoctorRepository extends JpaRepository<DoctorEntity, UUID> {
+
+    Optional<DoctorEntity> findById(UUID id);
 }
