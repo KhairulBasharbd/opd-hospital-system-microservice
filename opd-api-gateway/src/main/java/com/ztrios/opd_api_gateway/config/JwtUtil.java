fe44f073f1c376f6561bd47070860a506a9a11c2
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-24 15:48:45 +0600

    Add JWT Decoder Beans so that oauth2ResourceServer(oauth2.jwt()) finds the beans to decode jwt token

---
+++ opd-api-gateway/src/main/java/com/ztrios/opd_api_gateway/config/JwtUtil.java
@@ -0,0 +1,32 @@
+package com.ztrios.opd_api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class JwtUtil {
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        SecretKey key = new SecretKeySpec(
+                jwtSecret.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
+
+        return NimbusJwtDecoder
+                .withSecretKey(key)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}
