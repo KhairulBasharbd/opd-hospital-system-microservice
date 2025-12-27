688c7299591e997fbefeb01ae789d127f1663b4b
Khairul Basar <khairulbashar24853@gmail.com>
2025-12-28 01:27:25 +0600

    Change JwtUtil for reactive cloud gateway

--- opd-api-gateway/src/main/java/com/ztrios/opd_api_gateway/config/JwtUtil.java
+++ opd-api-gateway/src/main/java/com/ztrios/opd_api_gateway/config/JwtUtil.java
@@ -1,32 +1,39 @@
 package com.ztrios.opd_api_gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 
+import static com.nimbusds.jose.JWSAlgorithm.HS256;
+
+@Slf4j
 @Configuration
 public class JwtUtil {
     @Value("${security.jwt.secret}")
     private String jwtSecret;
 
-    @Bean
-    public JwtDecoder jwtDecoder() {
 
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
         SecretKey key = new SecretKeySpec(
                 jwtSecret.getBytes(StandardCharsets.UTF_8),
                 "HmacSHA256"
         );
 
-        return NimbusJwtDecoder
-                .withSecretKey(key)
+        log.debug("In ReactiveJwtDecoder ");
+        return NimbusReactiveJwtDecoder.withSecretKey(key)
                 .macAlgorithm(MacAlgorithm.HS256)
                 .build();
     }
+
 }
