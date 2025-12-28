f1fba636023e811fff920d825b6dda6dac1957b4
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-28 17:51:36 +0600

    Make changes DayofWeek type in DoctorScheduleResponse

--- opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/dto/DoctorScheduleResponse.java
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/dto/DoctorScheduleResponse.java
@@ -2,13 +2,14 @@ package com.ztrios.opd_doctor_service.dto;
 
 import com.ztrios.opd_doctor_service.enums.DaysOfWeek;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.UUID;
 
 public record DoctorScheduleResponse (
     UUID id,
     UUID doctorId,
-    DaysOfWeek daysOfWeek,
+    DayOfWeek daysOfWeek,
     LocalTime startTime,
     LocalTime endTime,
     int maxPatients,
