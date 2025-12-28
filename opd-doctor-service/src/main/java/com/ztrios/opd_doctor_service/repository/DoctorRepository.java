a8bcf2fadce316001066c62a37881026ca8ca2ad
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-28 17:55:21 +0600

    Add custom query for finding available doctors on a specific date and speciality

--- opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorRepository.java
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/repository/DoctorRepository.java
@@ -1,9 +1,12 @@
 package com.ztrios.opd_doctor_service.repository;
 
 import com.ztrios.opd_doctor_service.entity.DoctorEntity;
+import com.ztrios.opd_doctor_service.enums.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
+import java.time.DayOfWeek;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +15,14 @@ import java.util.UUID;
 public interface DoctorRepository extends JpaRepository<DoctorEntity, UUID> {
 
     Optional<DoctorEntity> findById(UUID id);
+
+    @Query("""
+        SELECT DISTINCT d
+        FROM DoctorEntity d
+        JOIN d.schedules s
+        WHERE d.specialization = :specialization
+          AND d.status = 'ACTIVE'
+          AND s.DayOfWeek = :dayOfWeek
+    """)
+    List<DoctorEntity> findAvailableDoctors(DayOfWeek dayOfWeek, Specialization specialization);
 }
