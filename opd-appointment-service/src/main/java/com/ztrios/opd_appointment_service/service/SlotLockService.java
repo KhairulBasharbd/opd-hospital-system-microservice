8aeab817a590fb6461d532f21ca0bc969ad1c07a
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-29 18:13:41 +0600

    Create SlotLockService in appointment Service

---
+++ opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/service/SlotLockService.java
@@ -0,0 +1,30 @@
+package com.ztrios.opd_appointment_service.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class SlotLockService {
+
+
+    private final StringRedisTemplate redisTemplate;
+
+
+    public boolean lockSlot(UUID doctorId, UUID scheduleId) {
+        String key = "slot:" + doctorId + ":" + scheduleId;
+        Boolean success = redisTemplate.opsForValue()
+                .setIfAbsent(key, "LOCKED", Duration.ofMinutes(15));
+        return Boolean.TRUE.equals(success);
+    }
+
+
+    public void releaseSlot(UUID doctorId, UUID scheduleId) {
+        redisTemplate.delete("slot:" + doctorId + ":" + scheduleId);
+    }
+}
