7420903d20545957c4db11c462eee84165910d0c
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-29 18:13:25 +0600

    Create GlobalExceptionHandler in appointment Service

---
+++ opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.ztrios.opd_appointment_service.exception;
+
+import com.ztrios.opd_appointment_service.exception.custom.AppointmentNotFoundException;
+import com.ztrios.opd_appointment_service.exception.custom.SlotNotAvailableException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(SlotNotAvailableException.class)
+    public ResponseEntity<Object> handleConflict(SlotNotAvailableException ex) {
+        return buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage());
+    }
+
+    @ExceptionHandler(AppointmentNotFoundException.class)
+    public ResponseEntity<Object> handleNotFound(AppointmentNotFoundException ex) {
+        return buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneric(Exception ex) {
+        return buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected error occurred");
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(HttpStatus status, String message) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", Instant.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", message);
+        return new ResponseEntity<>(body, status);
+    }
+
+}
