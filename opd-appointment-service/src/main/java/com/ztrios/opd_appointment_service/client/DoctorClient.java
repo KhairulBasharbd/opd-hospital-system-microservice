6111a9f53aadf14bb19f59caab2667d5d750b08e
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-29 18:11:37 +0600

    Create feign client for Doctor service in appointment Service

---
+++ opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/client/DoctorClient.java
@@ -0,0 +1,20 @@
+package com.ztrios.opd_appointment_service.client;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@FeignClient(name = "opd-doctor-service")
+public interface DoctorClient {
+
+    @GetMapping("/internal/doctors/{doctorId}/schedules/{scheduleId}/availability")
+    boolean isScheduleAvailable(@PathVariable UUID doctorId,
+                              @PathVariable UUID scheduleId,
+                              @RequestParam LocalDate date);
+
+}
