5cb4dbe8e62be28a6a10a9725b4e78dcfcb76a3d
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-29 18:12:08 +0600

    Make changes AppointmentRepository in appointment Service

--- opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/repository/AppointmentRepository.java
+++ opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/repository/AppointmentRepository.java
@@ -2,11 +2,13 @@ package com.ztrios.opd_appointment_service.repository;
 
 import com.ztrios.opd_appointment_service.entity.AppointmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface appointmentRepository extends JpaRepository<AppointmentEntity, UUID> {
+@Repository
+public interface AppointmentRepository extends JpaRepository<AppointmentEntity, UUID> {
 
     boolean existsByDoctorIdAndScheduleId(UUID doctorId, UUID scheduleId);
     Optional<AppointmentEntity> findByAppointmentSerialNo(String serialNo);
