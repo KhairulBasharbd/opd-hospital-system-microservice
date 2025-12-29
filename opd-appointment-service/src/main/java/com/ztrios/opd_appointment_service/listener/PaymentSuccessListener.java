83a9ed7109920fb48915461f7627dcda326b1a07
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-29 18:12:35 +0600

    Create PaymentSuccessListener in appointment Service

---
+++ opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/listener/PaymentSuccessListener.java
@@ -0,0 +1,22 @@
+package com.ztrios.opd_appointment_service.listener;
+
+
+import com.ztrios.opd_appointment_service.dto.PaymentSuccessEvent;
+import com.ztrios.opd_appointment_service.service.AppointmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentSuccessListener {
+
+
+    private final AppointmentService service;
+
+
+    @KafkaListener(topics = "PAYMENT_SUCCESS")
+    public void handle(PaymentSuccessEvent event) {
+        service.confirmAppointment(event.appointmentId());
+    }
+}
