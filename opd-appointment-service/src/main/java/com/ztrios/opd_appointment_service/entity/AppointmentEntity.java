6d5a8ecaaf4ebf7fe14b8e6c41bd528a82392e12
Khairul Basar <khairulbasar.dev@gmail.com>
2025-12-29 12:55:05 +0600

    Create AppointmentEntity in appointment Service

---
+++ opd-appointment-service/src/main/java/com/ztrios/opd_appointment_service/entity/AppointmentEntity.java
@@ -0,0 +1,64 @@
+package com.ztrios.opd_appointment_service.entity;
+
+
+import com.ztrios.opd_appointment_service.enums.AppointmentStatus;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "appointments", uniqueConstraints = {@UniqueConstraint(columnNames = {"patient_id","doctor_id","Schedule_id"})})
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+
+    @Column(name = "patient_id", nullable = false)
+    private UUID patientUserId;
+
+
+    @Column(name = "doctor_id",nullable = false)
+    private UUID doctorId;
+
+
+    @Column(name = "schedule_id",nullable = false)
+    private UUID scheduleId;
+
+
+    @Column(name = "appointment_date",nullable = false)
+    private LocalDate appointmentDate;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AppointmentStatus status;
+
+
+    @Column(name = "serial_no",nullable = false, unique = true)
+    private String serialNo;
+
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false )
+    private Instant createdAt = Instant.now();
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = true)
+    private Instant updatedAt;
+}
