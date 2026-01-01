ebedbc8e96cf25a507997492e467c77561c693b0
Khairul Basar <khairulbasar.dev@gmail.com>
2026-01-01 17:04:40 +0600

    Create DoctorInternalController in doctor  Service

---
+++ opd-doctor-service/src/main/java/com/ztrios/opd_doctor_service/controller/DoctorInternalController.java
@@ -0,0 +1,37 @@
+package com.ztrios.opd_doctor_service.controller;
+
+
+import com.ztrios.opd_doctor_service.service.DoctorAvailabilityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("/internal/doctors")
+@RequiredArgsConstructor
+public class DoctorInternalController {
+
+    private final DoctorAvailabilityService availabilityService;
+
+    @GetMapping("/{doctorId}/schedules/{scheduleId}/{serialNo}/availability")
+    public ResponseEntity<Boolean> checkAvailability(
+            @PathVariable UUID doctorId,
+            @PathVariable UUID scheduleId,
+            @PathVariable Integer serialNo,
+            @RequestParam LocalDate appointmentDate
+    ) {
+        boolean available = availabilityService.isScheduleAvailable(
+                doctorId,
+                scheduleId,
+                serialNo,
+                appointmentDate
+        );
+        return ResponseEntity.ok(available);
+    }
+}
+
