ab480304eeaa4002d00dd03a2723574de0f88dd1
Khairul Basar <khairulbasar.dev@gmail.com>
2026-01-07 18:17:01 +0600

    chore(billing) : 1. Add logger in produce event class 2. Configure kafka producer

--- opd-billing-service/src/main/java/com/ztrios/opd_billing_service/kafka/BillingEventProducer.java
+++ opd-billing-service/src/main/java/com/ztrios/opd_billing_service/kafka/BillingEventProducer.java
@@ -3,16 +3,21 @@ package com.ztrios.opd_billing_service.kafka;
 
 import com.ztrios.opd_billing_service.dto.PaymentSuccessEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class BillingEventProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void publishPaymentSuccess(PaymentSuccessEvent event) {
+
+        log.info("📤 PAYMENT_SUCCESS published: {}", event.appointmentId());
+
         kafkaTemplate.send("PAYMENT_SUCCESS", event.appointmentId().toString(), event);
     }
 }
